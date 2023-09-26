c2906fb3518fb3ce29b4ece5d32e9e5827bfb929
Radu-A <elcastravete@gmail.com>
2023-09-26 11:08:33 +0200

    changed/diet-article_height

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-
 // VARIABLES GLOBALES
 const recipesMain = document.querySelector(".recipes");
 const sectionSpa = document.createElement("section");
 const stepsSection = document.createElement("section");
 stepsSection.id = "steps-section";
-const startButton = document.getElementById('start-button');
+const startButton = document.getElementById("start-button");
 
 // VARIABLES CON LOS ENDPOINTS
 // Meet
-const meetUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=meat&number=4';
+const meetUrl =
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=meat&number=4";
 // Veggie
-const veggieUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian&number=4';
+const veggieUrl =
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian&number=4";
 // Vegan
-const veganUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegan&number=4";
+const veganUrl =
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegan&number=4";
 // Datos nutricionales con ID
-const nutritionUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/660109/nutritionWidget.json';
+const nutritionUrl =
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/660109/nutritionWidget.json";
 // Opciones método get
 const options = {
   method: "GET",
@@ -30,17 +33,17 @@ async function getRecipes() {
   try {
     let response = [];
     switch (recipesMain.id) {
-      case 'meet-main' :
+      case "meet-main":
         response = await fetch(meetUrl, options);
         break;
-      case 'veggie-main' :
+      case "veggie-main":
         response = await fetch(meetUrl, options);
         break;
-      case 'vegan-main' :
+      case "vegan-main":
         response = await fetch(meetUrl, options);
         break;
     }
-    const {recipes} = await response.json();
+    const { recipes } = await response.json();
     return recipes;
     console.log(randomRecipes);
   } catch (error) {
@@ -50,7 +53,10 @@ async function getRecipes() {
 async function getNutrition(id) {
   try {
     let response = [];
-    response = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/nutritionWidget.json`, options);
+    response = await fetch(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/nutritionWidget.json`,
+      options
+    );
     const nutrition = await response.json();
     return nutrition;
   } catch (error) {
@@ -61,23 +67,23 @@ async function getNutrition(id) {
 // Pintar lista
 function printList(array) {
   // Acorto los nombres que sean demasiado largos
-  let heading = '';
+  let heading = "";
   switch (recipesMain.id) {
-    case 'meet-main' :
-      heading = 'Meat'
+    case "meet-main":
+      heading = "Meat";
       break;
-    case 'veggie-main' :
-      heading = 'Veggie'
+    case "veggie-main":
+      heading = "Veggie";
       break;
-    case 'vegan-main' :
-      heading = 'Vegan'
+    case "vegan-main":
+      heading = "Vegan";
       break;
   }
   let titles = [];
-  array.forEach(element => {
-    titles.push(element.title.substr(0,22));
+  array.forEach((element) => {
+    titles.push(element.title.substr(0, 22));
   });
-  sectionSpa.id = "list-section"
+  sectionSpa.id = "list-section";
   sectionSpa.innerHTML = ` 
       <h1>${heading}</h1>
       <article class="suggestion" id="suggestion-1">
@@ -133,19 +139,19 @@ function printList(array) {
       </article>`;
   recipesMain.appendChild(sectionSpa);
   const backDietButton = document.getElementById("back-diet-button");
-  backDietButton.addEventListener('click', function() {
-    window.location = './diet.html';
-  })
+  backDietButton.addEventListener("click", function () {
+    window.location = "./diet.html";
+  });
 }
 // Pintar detalle
 function printDetail(array, i) {
   // Acorto los nombres que sean demasiado largos
   let titles = [];
-  array.forEach(element => {
-    titles.push(element.title.substr(0,22));
+  array.forEach((element) => {
+    titles.push(element.title.substr(0, 22));
   });
 
-  sectionSpa.id = 'detail-section';
+  sectionSpa.id = "detail-section";
   sectionSpa.innerHTML = `
     <h2>${titles[i]}</h2>
     <article class="detail-image">
@@ -170,56 +176,56 @@ function printDetail(array, i) {
   // Pintamos ingredientes
   const igQuantity = document.getElementById("ig-quantity");
   const igName = document.getElementById("ig-name");
-    array[i].extendedIngredients.forEach(element => {
-      const liQuantity = document.createElement('li');
-      const liName = document.createElement('li');
-      liQuantity.innerText = `${element.amount} ${element.unit}`;
-      liName.innerText = `${element.name}`;
-      igQuantity.appendChild(liQuantity);
-      igName.appendChild(liName);
-    })
+  array[i].extendedIngredients.forEach((element) => {
+    const liQuantity = document.createElement("li");
+    const liName = document.createElement("li");
+    liQuantity.innerText = `${element.amount} ${element.unit}`;
+    liName.innerText = `${element.name}`;
+    igQuantity.appendChild(liQuantity);
+    igName.appendChild(liName);
+  });
   // Gráfica con valor nutricional
   const nutritionValues = getNutrition(array[i].id);
-  nutritionValues.then(data=>{
-    console.log(data);
+  nutritionValues.then((data) => {
     const calories = data.nutrients[0].amount;
     const fat = data.nutrients[1].amount;
     const sat = data.nutrients[2].amount;
     const carb = data.nutrients[3].amount;
     const prot = data.nutrients[8].amount;
     console.log(calories);
-    const ctx = document.getElementById('myChart');
+    const ctx = document.getElementById("myChart");
     new Chart(ctx, {
-      type: 'bar',
+      type: "bar",
       data: {
-        labels: ['Fat', 'S.Fat', 'Carb', 'Prot'],
-        datasets: [{
-          label: 'gr',
-          data: [fat, sat, carb, prot],
-          borderWidth: 1
-        }]
+        labels: ["Fat", "S.Fat", "Carb", "Prot"],
+        datasets: [
+          {
+            label: "gr",
+            data: [fat, sat, carb, prot],
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         legend: {
-            display: false
+          display: false,
         },
         scales: {
           y: {
-            beginAtZero: true
-          }
-        }
-      }
+            beginAtZero: true,
+          },
+        },
+      },
     });
-  })
-  
-};
+  });
+}
 // Pintar instrucciones
 function printSteps(array, i) {
-  const directionsHeader = document.createElement('h2');
-  directionsHeader.innerHTML = 'Directions';
+  const directionsHeader = document.createElement("h2");
+  directionsHeader.innerHTML = "Directions";
   stepsSection.appendChild(directionsHeader);
   const articleBtn = document.createElement("article");
-  articleBtn.id = 'article-btn';
+  articleBtn.id = "article-btn";
   array[i].analyzedInstructions[0].steps.forEach((step, j) => {
     const stepArticle = document.createElement("article");
     stepArticle.classList.add("step-article");
@@ -228,64 +234,63 @@ function printSteps(array, i) {
 
     stepH3.innerText = `${step.number}`;
     stepP.innerText = `${step.step}`;
-    articleBtn.innerHTML = 
-    `<button id="back-button">Back to list</button>`;
+    articleBtn.innerHTML = `<button id="back-button">Back to list</button>`;
 
     stepArticle.appendChild(stepH3);
     stepArticle.appendChild(stepP);
-    
+
     stepsSection.appendChild(stepArticle);
     recipesMain.appendChild(stepsSection);
-  })
+  });
 
   // Botón volver atrás
-  
+
   stepsSection.appendChild(articleBtn);
   const backButton = document.getElementById("back-button");
-  backButton.addEventListener('click', function() {
-    window.location.hash = '';
-  })
+  backButton.addEventListener("click", function () {
+    window.location.hash = "";
+  });
 }
 
 // ----------NAVEGACIÓN-------------
-// 
+//
 // Pantalla inicial - Lista 4 recetas
 let randomRecipes = getRecipes();
 randomRecipes.then((data) => {
   if (recipesMain) {
     printList(data);
   }
-})
-// Pantalla detalle receta 
-window.addEventListener("hashchange", function() {
+});
+// Pantalla detalle receta
+window.addEventListener("hashchange", function () {
   let hash = window.location.hash;
-  sectionSpa.innerHTML = '';
+  sectionSpa.innerHTML = "";
   randomRecipes.then((data) => {
     switch (hash) {
-      case '' :
+      case "":
         randomRecipes.then((data) => {
           if (recipesMain) {
-            stepsSection.innerHTML = '';
+            stepsSection.innerHTML = "";
             stepsSection.remove();
             printList(data);
           }
-        })
-      case '#/first' :
+        });
+      case "#/first":
         printDetail(data, 0);
         printSteps(data, 0);
         break;
-      case '#/second' :
+      case "#/second":
         printDetail(data, 1);
         printSteps(data, 1);
         break;
-      case '#/third' :
+      case "#/third":
         printDetail(data, 2);
         printSteps(data, 2);
         break;
-      case '#/fourth' :
+      case "#/fourth":
         printDetail(data, 3);
         printSteps(data, 3);
         break;
     }
-  })
-})
+  });
+});
